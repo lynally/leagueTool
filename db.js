446376e7e20a505c6425546986df6bba945299ec
liuyn <2212770755@qq.com>
2022-12-12 14:42:40 +0800

    style 格式化代码

--- db.js
+++ db.js
@@ -1,33 +1,33 @@
-var mongoose = require("mongoose"); //引入mongoose
-var Schema = mongoose.Schema;
+var mongoose = require('mongoose') // 引入mongoose
+var Schema = mongoose.Schema
 
 
-var db = mongoose.connection;
-db.on('error', function callback() { //监听是否有异常
-  console.log("Connection error");
-});
-db.once('open', function callback() { //监听一次打开
-                                      //在这里创建你的模式和模型
-  console.log('connected!');
-});
+var db = mongoose.connection
+db.on('error', function callback() { // 监听是否有异常
+  console.log('Connection error')
+})
+db.once('open', function callback() { // 监听一次打开
+  // 在这里创建你的模式和模型
+  console.log('connected!')
+})
 db.on('connected', () => {
-  console.log('MongoDB connected success');
-});
+  console.log('MongoDB connected success')
+})
 db.on('disconnected', () => {
-  console.log('MongoDB connected disconnected');
-});
-mongoose.connect('mongodb://localhost/leagues');
+  console.log('MongoDB connected disconnected')
+})
+mongoose.connect('mongodb://localhost/leagues')
 
 // 创建一个联赛
 const league = Schema({
   name: String,
-  teams:Array,
-  startDate:String,
-  week:String,
-  dailyTime:String
-});
+  teams: Array,
+  startDate: String,
+  week: String,
+  dailyTime: String
+})
 
-mongoose.model('League', league);  // Schema编译成Model  (相当于Class（文档）)
+mongoose.model('League', league) // Schema编译成Model  (相当于Class（文档）)
 
 
-module.exports = mongoose;
+module.exports = mongoose
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express')
-const bp = require('body-parser');
-var logger = require('morgan');
-const app  = express();
+const bp = require('body-parser')
+var logger = require('morgan')
+const app = express()
 
 app.use(logger('dev'))
 
-app.use(bp.urlencoded({ extended: false }));
+app.use(bp.urlencoded({ extended: false }))
 
-app.use('/league', require('./controllers/api_league'));
+app.use('/league', require('./controllers/api_league'))
 
-app.get('/', function(req, res,next) {
-    res.send('Hello from root route. 111')
-});
+app.get('/', function(req, res, next) {
+  res.send('Hello from root route. 111')
+})
 
 
 /* istanbul ignore next */
 if (!module.parent) {
-    app.listen(3000);
-    console.log('Express started on port 3000');
+  app.listen(3000)
+  console.log('Express started on port 3000')
 }
